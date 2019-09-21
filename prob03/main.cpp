5eaa04f94e3b038a487767da3fda0fbadb23aada
alex90631 <alex90631@cs104u-2a.acad.fullerton.edu>
2019-09-21 14:12:18 -0700

    Problem 3 lab 5

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,25 @@
 // Random Rectangle
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+ unsigned seed = time(0);
+ srand (seed);
+
+ std::string box_chars;
+ int max_lenght = 40;
+ int lenght = rand() %max_lenght + 1;
+
+ box_chars.assign(lenght, '#');
+
+ std::cout << box_chars << std::endl;
+ std::cout << box_chars << std::endl;
+ std::cout << box_chars << std::endl;
+
+
+  return 0;
+}
